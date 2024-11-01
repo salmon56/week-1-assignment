a1b7307ccf9df6551ffdd8f597d0774d6e66dffd
salmon56 <145818828+salmon56@users.noreply.github.com>
2024-11-01 16:03:06 +0300

    Add files via upload
    assignment week 1

---
+++ user_input.py
@@ -0,0 +1,10 @@
+name = input("Enter your name: ")
+print("Hello,", name)
+
+age = input("whats is your age:")
+print()
+
+location = input("what is your current location:")
+print()
+
+print("Hello", name, "you are", age, "years old and live in", location)
